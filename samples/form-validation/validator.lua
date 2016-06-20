bfc12901164b767e5155d839a1d7d3eb122b7523
Shadaen <mangel.maxime@outlook.com>
2016-06-21 00:17:26 +0200

    WIP

--- samples/form-validation/validator.lua
+++ samples/form-validation/validator.lua
@@ -1,7 +1,9 @@
+--- @author Maxime Mangel (aka Shadaen)
 local window = js.global
 local document = window.document
 local ui
 
+--- Initialize the validator page.
 function init()
   local form = document:querySelector(".to-validate")
   if notNullJs(form) then
@@ -10,17 +12,30 @@ function init()
 
     ui.fields.pseudo.element = document:getElementById("pseudo")
 
+    for keyFields, field in pairs(ui.fields) do
+      field.element = document:getElementById(field.id)
+    end
+
   end
 end
 
-function isRequired(element)
-  return notEmpty(element.value), "Field should not be empty"
+--- Check if the field has been filled by the user.
+-- @param field Field table to check
+-- @return Return true if the value is not empty and return a msg string if there an error
+function isRequired(field)
+  return notEmpty(field.element.value), string.format("Field %s should not be empty", field.id)
 end
 
+--- Check if the value if not "null" from Javascript point of view.
+-- @param value Value to check
+-- @return Return true if the is not null or undefined
 function notNullJs(value)
   return value ~= js.null and value ~= nil
 end
 
+--- Check if the value is not empty from Javascript point of view.
+-- @param value Value to check
+-- @return Return true if the value is not empty
 function notEmpty(value)
   return value ~= "" and notNullJs(value)
 end
@@ -30,12 +45,19 @@ function validate(this, event)
 
   for keyFields, field in pairs(ui.fields) do
     for keys, values in pairs(field.rules) do
-      local result, msg = values(field.element)
-      print(result)
+      local result, msg = values(field)
       print(msg)
     end
   end
+end
 
+function isEmail(field)
+  local res = field.element.value:match("[A-Za-z0-9%.%%%+%-]+@[A-Za-z0-9%.%%%+%-]+%.%w%w%w?%w?")
+  if res then
+    return res
+  else
+    return res, string.format("Value : %s is not a valid email", field.element.value)
+  end
 end
 
 ui = {
@@ -52,21 +74,22 @@ ui = {
     email = {
       id = "email",
       rules = {
-
+        isRequired,
+        isEmail
       },
       element
     },
     password = {
       id = "password",
       rules = {
-
+        isRequired
       },
       element
     },
     password_conf = {
       id = "password_conf",
       rules = {
-
+        isRequired
       },
       element
     }
